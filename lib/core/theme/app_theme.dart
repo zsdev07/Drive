f1c6d41f428b476d59e7da984789831919a01e67
ZS DEV <251845909+zsdev07@users.noreply.github.com>
2026-03-29 20:09:41 +0530

    Update app_theme.dart

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -35,7 +35,7 @@ class AppTheme {
           ),
           iconTheme: IconThemeData(color: textPrimary),
         ),
-        cardTheme: CardTheme(
+        cardTheme: CardThemeData(
           color: bgCard,
           elevation: 0,
           shape: RoundedRectangleBorder(

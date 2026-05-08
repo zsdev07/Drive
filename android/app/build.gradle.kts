19013cf27e2194237fc432a35636b36dd6e87790
ZS DEV <251845909+zsdev07@users.noreply.github.com>
2026-05-08 17:22:48 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "zx.offical.drive"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
